be562e0c695162cb762cb2f1dcf0d7c99933e195
Vyacheslav <amondey34@gmail.com>
2024-12-18 23:05:00 +0500

    эксперименты  с modal
    нужно починить: modalOverlay click не работает ingredient-details

--- src/components/modal-overlay/modal-overlay.tsx
+++ src/components/modal-overlay/modal-overlay.tsx
@@ -1,6 +1,11 @@
+import { FC } from 'react';
 import styles from './styles.module.css'
 
-export const ModalOverlay = (props: {closeModal: () => void}) => {
+type Props = {
+    closeModal: () => void
+}
+
+export const ModalOverlay: FC<Props>= (props: Props) => {
     return (
         <div className={styles.modaloverlay} onClick={props.closeModal}></div>
     )

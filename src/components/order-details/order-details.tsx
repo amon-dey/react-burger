697c6340d5b9256309bb78fea291cb52f57b0b86
z79381 <z79381@main.ecp>
2024-12-19 10:12:41 +0500

    order-details icon

--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -1,16 +1,13 @@
-import { CheckMarkIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './styles.module.css'
 
-/*
-С иконкой надо какое-то колдуствао сделать            
-*/
+import { OrderDetailsSuccessIcon } from "./oreder-details-icon/oreder-details-icon"
 
-export const OrderDetails = ({orderNumber=99999}) =>  {    
+export const OrderDetails = ({ orderNumber = 99999 }) => {
     return (
         <section className={`${styles.items} mt-5 mb-30 mr-30 ml-30`}>
             <p className={`${styles.textglow} text text_type_digits-large mb-2`}>{orderNumber}</p>
             <p className="text text_type_main-medium">идентификатор заказа</p>
-            <CheckMarkIcon type="primary" className={`${styles.icon} m-15`} />
+            <OrderDetailsSuccessIcon />
             <p className="text text_type_main-default m-2">Ваш заказ начали готовить</p>
             <p className="text text_type_main-default text_color_inactive">Дождитесь готовности на орбитальной станции</p>
         </section>
